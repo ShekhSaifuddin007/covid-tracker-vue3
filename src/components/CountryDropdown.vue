bbf6a8e62de8185569508682ed7cfdcb41061201
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2021-04-29 18:24:02 +0600

    converting to vue 3

--- src/components/CountryDropdown.vue
+++ src/components/CountryDropdown.vue
@@ -6,21 +6,39 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
     name: "CountryDropdown",
     props: ['countries'],
-    data() {
+
+    setup({countries}, {emit}) {
+        const selected = ref(0)
+
+        function onChange() {
+            const country = countries.find((item) => item.ID === selected.value)
+
+            emit('get-country', country)
+        }
+
         return {
-            selected: 0
+            selected,
+            onChange
         }
     },
 
-    methods: {
-        onChange() {
-            const country = this.countries.find((item) => item.ID === this.selected)
+    // data() {
+    //     return {
+    //         selected: 0
+    //     }
+    // },
 
-            this.$emit('get-country', country)
-        }
-    }
+    // methods: {
+    //     onChange() {
+    //         const country = this.countries.find((item) => item.ID === this.selected)
+    //
+    //         this.$emit('get-country', country)
+    //     }
+    // }
 }
 </script>
